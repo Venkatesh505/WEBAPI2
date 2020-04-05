e0cd73001e3aa7043c285d968a087aab01abf310
venkatesh karamala <venkatesh.karamala@gmail.com>
2020-04-04 23:44:42 -0400

    first upload

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WEBAPI2.Models;
+using WEBAPI2.Services;
+
+namespace WEBAPI2.Controllers
+{
+
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+            {
+            this.contactRepository = new ContactRepository();
+            }
+        [AcceptVerbs("GET","POST")]
+        public Contact[] PullContacts()
+        {
+            var ctx = HttpContext.Current;
+            if(ctx != null)
+            {
+                return (Contact[])ctx.Cache["ContactStore"];
+            }
+            return new Contact[]
+        {
+            new Contact
+            {
+                Id = 0,
+                Name = "Placeholder"
+            }
+        };
+            //return this.contactRepository.AllContacts;
+        }
+        
+    }
+}
---
+++ Services/ContactRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WEBAPI2.Models;
+
+namespace WEBAPI2.Services
+{
+    public class ContactRepository
+    {
+        private const string CacheKey = "ContactStore";
+        public ContactRepository()
+            {
+            var ctx = HttpContext.Current;
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                        new Contact
+                        {
+                            Id=1,Name="First"
+
+                        },
+                        new Contact
+                        {
+                            Id=2,Name="Second"
+
+                        }
+                    };
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+            }
+        /*public Contact[] AllContacts => new Contact[]
+            {
+            new Contact
+            {
+                Id = 1,
+                Name = "Glenn Block"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "Dan Roth"
+            }
+            }; */
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+            if(ctx != null)
+            {
+                try
+                {
+                    var currentdata = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentdata.Add(contact);
+                    ctx.Cache[CacheKey] = currentdata.ToArray();
+                    return true;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+            return false;
+            
+        }
+    }
+}
